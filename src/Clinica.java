36bf638a65dd08c987846712b0a026caf54e2bfc
halangbacca <halanbacca@outlook.com>
2023-02-14 00:03:34 -0300

    IMC

--- src/Clinica.java
+++ src/Clinica.java
@@ -4,4 +4,20 @@ public class Clinica {
     String CNPJ;
     String logradouro;
     int numeroDaClinica;
+
+    public void calcularIMC(Double peso, Double altura) {
+        Double IMC = peso / (altura * altura);
+        if (IMC < 18.5) {
+            System.out.println(String.format("%.2f = Magreza", IMC));
+        } else if (IMC >= 18.5 && IMC <= 24.9) {
+            System.out.println(String.format("%.2f = Normal", IMC));
+        } else if (IMC >= 25.0 && IMC <= 29.9) {
+            System.out.println(String.format("%.2f = Sobrepeso", IMC));
+        } else if (IMC >= 30.0 && IMC <= 39.9) {
+            System.out.println(String.format("%.2f = Obesidade", IMC));
+        } else if (IMC > 40.0) {
+            System.out.println(String.format("%.2f = Obesidade grave", IMC));
+        }
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Clinica clinica = new Clinica();
+
+        clinica.calcularIMC(58.0, 1.50);
     }
 }
