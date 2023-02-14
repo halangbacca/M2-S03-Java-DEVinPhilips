e3ae58c2d0fb10fa5a0c89e7b6e33bd02ee4679b
halangbacca <halanbacca@outlook.com>
2023-02-13 22:16:04 -0300

    Classe Clínica

---
+++ README.md
@@ -0,0 +1,1 @@
+# M2-S03-Java-DEVinPhilips
---
+++ src/Clinica.java
@@ -0,0 +1,7 @@
+public class Clinica {
+    String nomeDoDono;
+    String nomeDaClinica;
+    String CNPJ;
+    String logradouro;
+    int numeroDaClinica;
+}
